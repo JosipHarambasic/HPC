2bad6f3633978d599763bc6643ee23a0acbdb829
JosipHarambasic <harambasic.josip97@gmail.com>
2022-03-16 16:08:01 +0100

    ex2

---
+++ exercise_sesion_03/hpc_esc_401_solutions/ex02/sum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "getTime.h"
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float r2;
+} coord;
+
+int main(int argc, char *argv[]) {
+    const int N = 1024*1024*256;
+    coord *data;
+    int i;
+    double sum, tStart, tEnd;
+    
+    data = malloc(N * sizeof(coord));
+    assert(data);
+    
+    sum = 0.0;
+    tStart = getTime();
+    for(i=0; i<N; ++i) {
+        data[i].x = i & 31;
+        data[i].y = i & 63;
+        data[i].z = i & 15;
+        data[i].r2 = data[i].x*data[i].x + data[i].y*data[i].y + data[i].z*data[i].z;
+        sum += sqrt(data[i].r2);
+    }
+    tEnd = getTime() - tStart;
+    printf("sum=%f\n", sum);
+    printf("Computed in: %f\n", tEnd); 
+    return 0;
+}

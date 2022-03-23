6c938273f37f1f9c9983f66ecc8fe6d05726bf55
JosipHarambasic <harambasic.josip97@gmail.com>
2022-03-23 19:34:33 +0100

    primenumbers check

---
+++ exsession04/primenumbers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+for((i=2; i <= "$1"/2; i++))
+do
+	if [ $(($1%i)) -eq 0  ]
+	then
+		echo "$1 is not a prime number."
+		exit
+	fi
+done
+echo "$1 is a prime number"
--- exsession04/scrpit.sh
+++ exsession04/scrpit.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-cat $1 | while read line 
-do 	
-	echo $line
-	echo -e "\n"
-done
+
+if [ -f $1 ] && [ ! -z $1 ] ; 
+then
+	cat $1 | while read line 
+	do 	
+		echo $line
+		echo -e "\n"
+	done
+else 
+	echo "$1 does not exist."
+fi
 
 
 

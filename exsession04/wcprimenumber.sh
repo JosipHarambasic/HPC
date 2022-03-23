10e62b86f10e0543dbce859c6aa98a4cff5c1d67
JosipHarambasic <harambasic.josip97@gmail.com>
2022-03-23 22:20:26 +0100

    wc prime

--- exsession04/wcprimenumber.sh
+++ exsession04/wcprimenumber.sh
@@ -3,14 +3,17 @@
 if ! [ $# -eq 1  ]; 
 then
 	echo "Too much arguments only 1 allowed!"
-	exit 
-fi
-
-factors=$(factor $1 | wc -w)
-
-if [ $factors -gt 2 ];
+	exit
+elif  [[ $1 ]] && [ $1 -eq $1 ];
 then
-	echo "$1 is composite." 
-else
-	echo "$1 is prime."
+	factors=$(factor $1 | wc -w)
+
+	if [ $factors -gt 2 ]; 
+	then
+		echo "$1 is composite." 
+	else
+		echo "$1 is prime." 
+	fi
+else 
+	echo "$1 is not a number."	
 fi

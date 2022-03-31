37a3441d5d727e3e732845c92427212db719d7f7
JosipHarambasic <harambasic.josip97@gmail.com>
2022-03-31 19:44:04 +0200

    update

--- exsession05/ex03/poisson_solver/jacobi.cpp
+++ exsession05/ex03/poisson_solver/jacobi.cpp
@@ -36,7 +36,7 @@ void jacobi_step(params p, double** u_new, double** u_old, double** f){
     for (int i=1; i<p.nx-1; i++){
         for (int j=1; j<p.ny-1; j++){
             u_old[i][j] = u_new[i][j];
-            u_new[i][j] = 0.25*(u_old[i-1][j]+ u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1]-(dx*dy));
+            u_new[i][j] = 0.25*(u_old[i-1][j]+ u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1]-(dx*dy)); //here should be f[i][j], but it does not work somehow, only if we increase the tolerance to 1.0-e30 or something like this then it works
         }
     }
 
